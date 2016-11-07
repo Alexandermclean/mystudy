38b64d4a9531a9f696bc579b59abbdf80da03bcd
yuzhenghui <779186016@qq.com>
2016-11-07 14:57:41 +0800

    onload

--- waterfall/waterfall.js
+++ waterfall/waterfall.js
@@ -11,9 +11,7 @@ $(document).ready(function(){
 				waterfall();
 		}
 	})
-})
-
-function waterfall(){
+	function waterfall(){
 	var $boxs=$("#main>div")//选择main下的一级子标签
 	var w=$boxs.eq(0).outerWidth();//width()获取设置的宽度，outerWidth获取border、padding等加起来的宽度
 	var cols=Math.floor($(window).width()/w	);
@@ -48,3 +46,6 @@ function checkScrollSlide(){
 	var documentH=$(window).height();
 	reture(lastBoxdDis<scrollTop+documentH)?true:false;
 }
+
+})
+
