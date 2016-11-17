df3d5d23c9fa6b3e90f257caf78bdc72f9691408
yuzhenghui <779186016@qq.com>
2016-11-17 14:51:49 +0800

    PAGE

--- NetEase_try/js/main.js
+++ NetEase_try/js/main.js
@@ -1,11 +1,17 @@
 
 $(document).ready(function() {
 	loadData(1,10);
+	var pageLi=$('.course-page-ol li');
+	//var pageIndex=$('.course-page-ol li.li-checked')[0].Index;
+
 	$('#program-language-btn').on('click',function(){
 		$(this).addClass('toGreen');
 		$('#product-design-btn').removeClass('toGreen');
 		$('#course-listtt').empty();
 		loadData(1,20);
+		pageLi.removeClass('li-checked');
+	    $(pageLi[2]).addClass('li-checked');
+	    pageIndex=$('.course-page-ol li.li-checked')[0].Index;
 	});//编程语言按钮点击事件
 
 	$('#product-design-btn').on('click',function(){
@@ -13,7 +19,68 @@ $(document).ready(function() {
 		$('#program-language-btn').removeClass('toGreen');
 		$('#course-listtt').empty();
 		loadData(1,10);
+		pageLi.removeClass('li-checked');
+	    $(pageLi[2]).addClass('li-checked');
+	    pageIndex=$('.course-page-ol li.li-checked')[0].Index;
 	});//产品设计按钮点击事件
+
+	/*course-page*/
+	for(var i=0;i<pageLi.length;i++){
+		pageLi[i].Index=i;
+		pageLi.eq(i).click(function(){
+			//console.log(this.Index);
+			pageLi.removeClass('li-checked');
+			$(this).addClass('li-checked');
+			$('#course-listtt').empty();
+			if($('#product-design-btn').hasClass('toGreen')){
+	         	loadData(3-this.Index,10);
+	         }
+	         else{
+	         	loadData(3-this.Index,20);
+	         }
+	         pageIndex=$('.course-page-ol li.li-checked')[0].Index;
+		});
+	}
+	pageIndex=$('.course-page-ol li.li-checked')[0].Index;
+
+	$('.course-page-back').on('click',function(){
+		if(pageIndex==2){
+			return;
+		}
+		else{
+	         pageIndex++;
+	         $('#course-listtt').empty();
+	         if($('#product-design-btn').hasClass('toGreen')){
+	         	loadData(3-pageIndex,10);
+	         }
+	         else{
+	         	loadData(3-pageIndex,20);
+	         }
+	         pageLi.removeClass('li-checked');
+	         $(pageLi[pageIndex]).addClass('li-checked');
+	         //console.log(pageLi[5-pageIndex]);
+		}
+	});
+	//pageIndex=$('.course-page-ol li.li-checked')[0].Index;
+	$('.course-page-front').on('click',function(){
+		if(pageIndex==0){
+			return;
+		}
+		else{
+	         pageIndex--;
+	         $('#course-listtt').empty();
+	         if($('#product-design-btn').hasClass('toGreen')){
+	         	loadData(3-pageIndex,10);
+	         }
+	         else{
+	         	loadData(3-pageIndex,20);
+	         }
+	         pageLi.removeClass('li-checked');
+	         $(pageLi[pageIndex]).addClass('li-checked');
+	         //console.log(pageLi[5-pageIndex]);
+		}
+	});
+
 });
 
 function loadData(Data_pageNo,Data_type){
@@ -28,62 +95,28 @@ function loadData(Data_pageNo,Data_type){
 			},
 			dataType:'json',
 			success:function(_data){
-				//console.log(_data[2].bigPhotoUrl);
-				//console.log(_data.list.length);
+				var htmStr='';
 				for(var i=0;i<_data.list.length;i++){
-						var ID='#'+'course-parentDiv'+i;
-						//var name_ID='#'+'course-name'+i;
-						//console.log(ID);
-						//console.log(name_ID);
-						
-						$('body').on('mouseover',ID,function(){
-							$(this).find('span').eq(0).css('color','#39a030');
-						});
-						$('body').on('mouseout',ID,function(){
-							$(this).find('span').eq(0).css('color','#333333');
-						});														//事件代理
-						var parentDiv=$('<div></div>');
-						parentDiv.attr('id','course-parentDiv'+i);
-						parentDiv.addClass('course-list-div');
-						//console.log(ID);
-						var _img=$('<img>');
-						_img.attr('src',_data.list[i].middlePhotoUrl);
-						_img.addClass('course-list-img');
-						parentDiv.append(_img);
+						if(_data.list[i].price==0){
+							_data.list[i].price='免费';
+						}
+						else{
+							_data.list[i].price='￥'+_data.list[i].price;
+						}
 
-						var _name=$('<span></span>');
-						_name.html(_data.list[i].name);
-						//_name.attr('id','course-name'+i);
-						_name.addClass('course-list-name');
-						parentDiv.append(_name);
-		
-						var _provider=$('<span></span>');
-						_provider.html(_data.list[i].provider);
-						_provider.addClass('course-list-provider');
-						parentDiv.append(_provider);
-
-						var _learnerCount=$('<span></span>');
-						_learnerCount.html(_data.list[i].learnerCount);
-						_learnerCount.addClass('course-list-learnerCount');
-						parentDiv.append(_learnerCount);
-
-						var _price=$('<span></span>');
-							if(_data.list[i].price==0){
-								_price.html('免费');
-							}
-							else{
-								_price.html('¥'+_data.list[i].price);
-							}
-						_price.addClass('course-list-price');
-						parentDiv.append(_price);
-
-						$('#course-listtt').append(parentDiv);
-					}
-					
-				},
+					htmStr+='<div class="course-list-div">'+
+							'<img class="course-list-img" src='+_data.list[i].middlePhotoUrl+'>'+
+							'<span class="course-list-name">'+_data.list[i].name+'</span>'+
+							'<span class="course-list-provider">'+_data.list[i].provider+'</span>'+
+							'<span class="course-list-learnerCount">'+_data.list[i].learnerCount+'</span>'+
+							'<span class="course-list-price">'+_data.list[i].price+'</span>'+
+							'</div>';
+				}
+				$('#course-listtt').append(htmStr);
+			},
 			error:function(jqXHR){
 				console.log(jqXHR.status);
-			}
+			},
 		});
 }//ajax数据请求
 		
