97fdb3d6e64923ef0299f8cbf9c4e2993a9b6fd5
yuzhenghui <779186016@qq.com>
2017-01-11 17:39:51 +0800

    waterfall

--- waterfall/waterfall.js
+++ waterfall/waterfall.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	waterfall();
 	var dataInt={'data':[{'src':'25.jpg'},{'src':'26.jpg'},{'src':'27.jpg'},{'src':'28.jpg'}]};
 	$(window).on('scroll',function(){
-		if (checkScrollSlide) {
+		if (checkScrollSlide()) {
 			$.each(dataInt.data,function(key,value){
 				var oBox=$('<div>').addClass('box').appendTo($('#main'));
 				var oPic=$('<div>').addClass('pic').appendTo($(oBox));
@@ -18,7 +18,7 @@ function waterfall(){
 	var $boxs=$("#main>div")//选择main下的一级子标签
 	var w=$boxs.eq(0).outerWidth();//width()获取设置的宽度，outerWidth获取border、padding等加起来的宽度
 	var cols=Math.floor($(window).width()/w	);
-	//console.log(cols);
+	console.log(cols);
 	$("#main").width(cols*w).css("margin","0 auto");
 	var hArr=[];
 	$boxs.each(function(index,value){//index:遍历的元素索引 value:遍历的元素
@@ -44,9 +44,9 @@ function waterfall(){
 
 function checkScrollSlide(){
 	var lastBox=$('#main>div').last();
-	var lastBoxdDis=$lastBox.offset().top+Math.floor($lastBox.outerHeight()/2);
+	var lastBoxdDis=lastBox.offset().top+Math.floor(lastBox.outerHeight()/2);
 	var scrollTop=$(window).scrollTop();
 	var documentH=$(window).height();
-	reture(lastBoxdDis<scrollTop+documentH)?true:false;
+	return (lastBoxdDis<scrollTop+documentH)?true:false;
 }
 
