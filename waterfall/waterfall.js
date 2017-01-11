2d49dc869203414966f78184e236fccd47ae2fff
yuzhenghui <779186016@qq.com>
2017-01-11 17:59:41 +0800

    again

--- waterfall/waterfall.js
+++ waterfall/waterfall.js
@@ -35,7 +35,7 @@ function waterfall(){
 				'top':minH+'px',
 				'left':minHIndex*w+'px',
 			})
-			hArr[minHIndex]+=$boxs.eq(index).outerHeight();//修改数组，避免图片重叠
+			hArr[minHIndex]+=h;//修改数组，避免图片重叠
 		}
 
 	})
